f886546888bebfa9021c6eb334e763dc7fc6eaba
Mohid-Abdullah <141454033+Mohid-Abdullah@users.noreply.github.com>
2025-06-29 18:28:57 +0500

    Update and rename firstmodel.py to DiabetesLinearRegression.py

--- DiabetesLinearRegression.py
+++ DiabetesLinearRegression.py
@@ -4,19 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load diabetes dataset
+# Loading diabetes dataset
 diabetes = load_diabetes()
 
-# Create DataFrame
 df = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
 df['target'] = diabetes.target
 
 print(df.head())
 
-X = df.drop('target', axis=1)  # Features
-y = df['target']               # Labels
+X = df.drop('target', axis=1)
+y = df['target']            
 
-# Split into 80% train, 20% test
+# Spliting into testing an training
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
@@ -30,6 +29,6 @@ r2 = r2_score(y_test, y_pred)
 print(f"Mean Squared Error: {mse:.2f}")
 print(f"R^2 Score: {r2:.2f}")
 
-print(model.coef_)   # The weights (slopes)
-print(model.intercept_)  # The intercept (bias)
+print(model.coef_)
+print(model.intercept_) 
 
